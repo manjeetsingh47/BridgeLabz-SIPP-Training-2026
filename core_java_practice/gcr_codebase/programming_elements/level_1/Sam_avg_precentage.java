d2cb857ff0d7f523d921626d1f55f4afd33ef79b
manjeetsingh47 <7347nmanjeet@gmail.com>
2026-06-11 12:30:14 +0530

    [Manjeet] added programming_elements level_1 problem (Sams average percentage)

---
+++ core_java_practice/gcr_codebase/programming_elements/level_1/Sam_avg_precentage.java
@@ -0,0 +1,15 @@
+package core_java_practice.gcr_codebase.programming_elements.level_1;
+
+public class Sam_avg_precentage {
+    public static void main(String[] args) {
+        float maths=94;
+        float physics=95;
+        float chemistry=96;
+
+        float totalMarks=300;
+
+        float avg=((maths+physics+chemistry)/totalMarks)*100;
+        System.out.print(avg);
+
+    }
+}
