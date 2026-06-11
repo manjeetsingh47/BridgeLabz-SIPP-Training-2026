1673b84ab5ec189a36775fa8fa9276e5e4be2c14
manjeetsingh47 <7347nmanjeet@gmail.com>
2026-06-11 12:44:12 +0530

    [Manjeet] added programming_elements level_1 problem (Pens Distribution)

---
+++ core_java_practice/gcr_codebase/programming_elements/level_1/PensDistribution.java
@@ -0,0 +1,15 @@
+package core_java_practice.gcr_codebase.programming_elements.level_1;
+
+public class PensDistribution {
+    public static void main(String[] args) {
+
+        int totalPens = 14;
+        int students = 3;
+
+        int pensPerStudent = totalPens / students;
+        int remainingPens = totalPens % students;
+
+        System.out.println("The Pen Per Student is " + pensPerStudent +
+                " and the remaining pen not distributed is " + remainingPens);
+    }
+}
