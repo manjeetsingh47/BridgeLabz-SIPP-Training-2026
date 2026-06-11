8f9017fc4c915bb8b9a0d2847295d7ca0a5e41f9
manjeetsingh47 <7347nmanjeet@gmail.com>
2026-06-11 12:24:26 +0530

    [Manjeet] added programming_elements level_1 problem (Harry age)

---
+++ core_java_practice/gcr_codebase/programming_elements/level_1/Age_of_harry.java
@@ -0,0 +1,10 @@
+package core_java_practice.gcr_codebase.programming_elements.level_1;
+
+public class Age_of_harry {
+    public static void main(String[] args) {
+        int birthYear=2000;
+        int currentYear=2024;
+
+        System.out.println("Harry's age in 2024 is "+(currentYear-birthYear));
+    }
+}
