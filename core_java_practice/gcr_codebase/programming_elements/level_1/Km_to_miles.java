6a0f5137a29c3d56487d50ca781b4aaaa2b8fa1f
manjeetsingh47 <7347nmanjeet@gmail.com>
2026-06-11 12:33:30 +0530

    [Manjeet] added programming_elements level_1 problem (Km to miles)

---
+++ core_java_practice/gcr_codebase/programming_elements/level_1/Km_to_miles.java
@@ -0,0 +1,9 @@
+package core_java_practice.gcr_codebase.programming_elements.level_1;
+
+public class Km_to_miles {
+    public static void main(String[] args) {
+        double km=10.8;
+        double miles=(10.8)*(1.6);
+        System.out.println("The distance "+ km +" km in miles is "+ miles);
+    }
+}
