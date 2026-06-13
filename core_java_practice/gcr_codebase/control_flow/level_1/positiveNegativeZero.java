46f6530993831dafcc1d135fc1744e72ab774161
manjeetsingh47 <7347nmanjeet@gmail.com>
2026-06-13 20:45:27 +0530

    [Manjeet] added control_flow  level_1 problem (positive negative zero)

---
+++ core_java_practice/gcr_codebase/control_flow/level_1/positiveNegativeZero.java
@@ -0,0 +1,21 @@
+package core_java_practice.gcr_codebase.control_flow.level_1;
+
+import java.util.Scanner;
+
+public class positiveNegativeZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        sc.close();
+
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
