87c9e41e6290fd84d5a917fa0666c0f703915d6b
manjeetsingh47 <7347nmanjeet@gmail.com>
2026-06-13 20:44:08 +0530

    [Manjeet] added control_flow  level_1 problem (person can vote)

---
+++ core_java_practice/gcr_codebase/control_flow/level_1/votingEligibility.java
@@ -0,0 +1,19 @@
+package core_java_practice.gcr_codebase.control_flow.level_1;
+
+import java.util.Scanner;
+
+public class votingEligibility {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter age: ");
+        int age = sc.nextInt();
+        sc.close();
+
+        if (age >= 18) {
+            System.out.println("The person's age is " + age + " and can vote.");
+        } else {
+            System.out.println("The person's age is " + age + " and cannot vote.");
+        }
+    }
+}
