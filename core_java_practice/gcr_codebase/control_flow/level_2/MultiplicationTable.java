1f190798b9429cd5ea6f6ea5f28856c9dc666217
manjeetsingh47 <7347nmanjeet@gmail.com>
2026-06-13 20:57:51 +0530

    [Manjeet] added control_flow  level_1 problem (Multiplication table)

---
+++ core_java_practice/gcr_codebase/control_flow/level_2/MultiplicationTable.java
@@ -0,0 +1,18 @@
+package core_java_practice.gcr_codebase.control_flow.level_2;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+
+        sc.close();
+    }
+}
