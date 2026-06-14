929190c638508fb0512c5a2808167bc9a5d8ee9c
manjeetsingh47 <7347nmanjeet@gmail.com>
2026-06-14 07:51:01 +0530

    [Manjeet] added Arrays  level_1  problem (day of week)

---
+++ core_java_practice/gcr_codebase/Arrays/level_1/VotingEligibility.java
@@ -0,0 +1,32 @@
+package core_java_practice.gcr_codebase.Arrays.level_1;
+
+import java.util.Scanner;
+
+public class VotingEligibility {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] ages = new int[10];
+
+        System.out.println("Enter ages of 10 students:");
+
+        for (int i = 0; i < ages.length; i++) {
+            ages[i] = sc.nextInt();
+        }
+
+        System.out.println("\nVoting Eligibility:");
+
+        for (int i = 0; i < ages.length; i++) {
+
+            if (ages[i] < 0) {
+                System.out.println("Invalid age: " + ages[i]);
+            } else if (ages[i] >= 18) {
+                System.out.println("The student with age " + ages[i] + " can vote.");
+            } else {
+                System.out.println("The student with age " + ages[i] + " cannot vote.");
+            }
+        }
+
+        sc.close();
+    }
+}
