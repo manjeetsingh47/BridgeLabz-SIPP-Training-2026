f34a399a318d8385a58db4fea6ea150cef26fa3d
manjeetsingh47 <7347nmanjeet@gmail.com>
2026-06-17 08:55:34 +0530

    [Manjeet] added Methods level_1  problem (sum of natural number)

---
+++ core_java_practice/gcr_codebase/Methods/level_1/SumNaturalNumbers.java
@@ -0,0 +1,27 @@
+package core_java_practice.gcr_codebase.Methods.level_1;
+
+import java.util.Scanner;
+
+public class SumNaturalNumbers {
+
+    public static int findSum(int n) {
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.close();
+
+        System.out.println("Sum = " + findSum(n));
+    }
+}
