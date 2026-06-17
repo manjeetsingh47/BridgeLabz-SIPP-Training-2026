0f2e16c8aa5e2dcf7a35d7719e19b1ecd4bfa7e1
manjeetsingh47 <7347nmanjeet@gmail.com>
2026-06-17 08:52:23 +0530

    [Manjeet] added Methods level_1  problem (Athlete run)

---
+++ core_java_practice/gcr_codebase/Methods/level_1/AthleteRun.java
@@ -0,0 +1,22 @@
+package core_java_practice.gcr_codebase.Methods.level_1;
+
+import java.util.Scanner;
+
+public class AthleteRun {
+
+    public static double calculateRounds(double a, double b, double c) {
+        double perimeter = a + b + c;
+        return 5000 / perimeter;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+        System.out.println("Rounds Required = "
+                + calculateRounds(a, b, c));
+    }
+}
