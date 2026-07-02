d9eb9245278c37dc00a80285774ec403cb2da7bf
manjeetsingh47 <7347nmanjeet@gmail.com>
2026-07-02 08:47:57 +0530

    [manjeet] added collections framework scenario based question (Event Entry Verification System)

---
+++ oops_practice/gcr_codebase/Collection_Framework/scenario_based_problem/EventEntry.java
@@ -0,0 +1,35 @@
+package oops_practice.gcr_codebase.Collection_Framework.scenario_based_problem;
+
+import java.util.*;
+
+public class EventEntry {
+
+    static HashSet<String> participants = new HashSet<>();
+
+    static void register(String email) {
+
+        if (participants.add(email))
+            System.out.println("Registration Successful.");
+        else
+            System.out.println("Duplicate Registration Rejected.");
+    }
+
+    static void display() {
+
+        System.out.println("\nRegistered Participants:");
+
+        for (String email : participants)
+            System.out.println(email);
+
+        System.out.println("Total Participants: " + participants.size());
+    }
+
+    public static void main(String[] args) {
+
+        register("rahul@gmail.com");
+        register("amit@gmail.com");
+        register("rahul@gmail.com");
+
+        display();
+    }
+}
