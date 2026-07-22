b0971832edbda1a433d4504b343120f6c77adaa4
manjeetsingh47 <7347nmanjeet@gmail.com>
2026-07-22 19:31:36 +0530

    [Manjeet] dsa basics trees )

---
+++ DSA_Basics/gcr_codebase/Trees/HeightOfTree.java
@@ -0,0 +1,39 @@
+package DSA_Basics.gcr_codebase.Trees;
+
+public class HeightOfTree {
+    static class Node {
+
+        int val;
+        Node left, right;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    static int height(Node node) {
+
+        if (node == null)
+            return -1;
+
+        return Math.max(height(node.left), height(node.right)) + 1;
+    }
+
+    static boolean isTooDeep(Node root, int threshold) {
+
+        return height(root) > threshold;
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.left.left = new Node(3);
+        root.left.left.left = new Node(4);
+
+        int threshold = 2;
+
+        System.out.println("Height = " + height(root));
+        System.out.println(isTooDeep(root, threshold));
+    }
+}
---
+++ DSA_Basics/gcr_codebase/Trees/IterativePreOrderTraversal.java
@@ -0,0 +1,51 @@
+package DSA_Basics.gcr_codebase.Trees;
+
+import java.util.*;
+
+public class IterativePreOrderTraversal {
+    static class Node {
+        int val;
+        Node left, right;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    static List<Integer> iterativePreorder(Node root) {
+
+        List<Integer> result = new ArrayList<>();
+
+        if (root == null)
+            return result;
+
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+
+            Node node = stack.pop();
+
+            result.add(node.val);
+
+            if (node.right != null)
+                stack.push(node.right);
+
+            if (node.left != null)
+                stack.push(node.left);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        System.out.println(iterativePreorder(root));
+    }
+}
